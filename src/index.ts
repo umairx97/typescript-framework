a9ebff9426e9a26c1e247f8b6bf563e807ccc6dd
umairx97 <umair.bajwa@hotmail.com>
2020-05-23 18:27:58 +0500

    feat: Add events class

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,7 @@
 import { User } from './models/User'
 
-const user = new User({ name: 'Umair', age: 20 })
-user.on('change', () => { 
-  console.log('USER CHANGED')
-})
+const user = new User({ name: 'umair', age: 22, id: 1 })
 
-user.trigger('change')
+user.fetch()
+
+console.log(user)
--- src/models/User.ts
+++ src/models/User.ts
@@ -1,35 +1,41 @@
+import axios, { AxiosResponse } from 'axios';
+
 interface UserProps {
   name?: string
   age?: number
+  id?: number
 }
 
-type Callback = () => void
+const BASE_URL = `http://localhost:3000`
 
 export class User {
-  events: { [key: string]: Callback[] } = {}
+  constructor(private data: UserProps) { }
 
-  constructor(private data: UserProps) {
-
-  }
+  // Get the property name from this.data either returns a string or a number 
   get(propName: string): number | string {
     return this.data[propName]
   }
 
+  // Sets a property on this.data 
   set(update: UserProps): void {
     Object.assign(this.data, update)
   }
 
-  on(eventName: string, callback: Callback): void {
-    const handlers = this.events[eventName] || []
-    handlers.push(callback)
-    this.events[eventName] = handlers
+  // fetch the current user with an id
+  fetch(): void {
+    axios.get(`${BASE_URL}/users/${this.get('id')}`)
+      .then((response: AxiosResponse): void => {
+        this.set(response.data)
+      })
   }
 
-  trigger(eventName: string): void {
-    const handlers = this.events[eventName] || []
-    if (!handlers || handlers.length === 0) return
-    handlers.forEach(callback => callback())
+  // Save data inside json server if the users already exists or create a new one 
+  save(): void {
+    const userId = this.get('id')
+    if (userId) {
+      axios.put(`${BASE_URL}/users/${userId}`, this.data)
+    } else {
+      axios.post(`${BASE_URL}/users`, this.data)
+    }
   }
-
-
 }
